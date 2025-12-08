31035d1116b56065499c09df6a613a2266a18fab
DianaB177 <anaidb77@gmail.com>
2025-12-08 20:21:52 +0100

    url

--- src/components/MisRetos.tsx
+++ src/components/MisRetos.tsx
@@ -92,7 +92,7 @@ export default function MisRetos() {
       };
 
       await axios.put(
-        `https://techreto-back-production.up.railway.app/api/${inscripcionActual.idInscripcion}`,
+        `https://techreto-back-production.up.railway.app/api/inscripciones/${inscripcionActual.idInscripcion}`,
         payload
       );
 
