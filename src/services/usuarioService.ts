7775a6a2f0baf533ac6b630989882716ef90b749
DianaB177 <anaidb77@gmail.com>
2025-12-08 22:31:02 +0100

    update

--- src/services/usuarioService.ts
+++ src/services/usuarioService.ts
@@ -17,22 +17,9 @@ export async function loginUsuario(
 
 // REGISTRO
 export async function registerUsuario(usuario: Usuario): Promise<Usuario> {
-  const payload = {
-    nombre: usuario.nombre,
-    email: usuario.email,
-    password: usuario.password,
-    rol: usuario.rol,
-    competencias: usuario.rol === "PARTICIPANTE" ? usuario.competencias : null
-  };
-
-  const response = await axios.post<Usuario>(
-    "https://techreto-back-production.up.railway.app/api/usuarios",
-    payload
-  );
-
+  const response = await axios.post<Usuario>(API_URL, usuario);
   return response.data;
 }
-
 
 // OBTENER TODOS
 export async function getUsuarios(): Promise<Usuario[]> {
